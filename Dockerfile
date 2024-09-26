1907267dd695172d06837c59461619cfd84c6924
lxzLocus <75218672+lxzLocus@users.noreply.github.com>
2024-09-26 15:26:41 +0900

    [WIP] front grpc対応

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,10 @@ RUN npm install -g next@14.2.4
 
 RUN yarn add @radix-ui/themes
 RUN yarn add @radix-ui/react-icons
+RUN yarn add @grpc/grpc-js
+RUN yarn add @grpc/proto-loader
+RUN yarn add grpc-web google-protobuf
+RUN yarn add grpc-tools grpc_tools_node_protoc_ts
 RUN yarn add fs
 
 COPY . .
--- src/features/api/FetchScholarInfo.ts
+++ src/features/api/FetchScholarInfo.ts
@@ -1,22 +1,35 @@
+import { loadPackageDefinition, credentials } from '@grpc/grpc-js';
+import { loadSync } from '@grpc/proto-loader';
+
+
+// Protobufの定義ファイルを読み込む
+const PROTO_PATH = './service.proto';
+const packageDefinition = loadSync(PROTO_PATH, {
+	keepCase: true,
+	longs: String,
+	enums: String,
+	defaults: true,
+	oneofs: true
+});
+
+const serviceProto = loadPackageDefinition(packageDefinition).your_service_package as any;
+
+// gRPCクライアントを作成する
+const client = new serviceProto.SearchScholar(
+	`${process.env.NEXT_PUBLIC_API_URL}`,
+	credentials.createInsecure()
+);
+
 const FetchScholarInfo = async (query: string) => {
-	try {
-		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/execute?params=scholar`, {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json'
-			},
+	return new Promise((resolve, reject) => {
+		client.Search({ query: query }, (error: any, response: any) => {
+			if (error) {
+				reject(new Error('論文の取得に失敗しました'));
+			} else {
+				resolve(response);
+			}
 		});
-
-		if (!response.ok) {
-			throw new Error('論文の取得に失敗しました');
-		}
-
-		const data = await response.json();
-
-		return data;
-	} catch (error) {
-		return { 'error': error };
-	}
+	});
 }
 
 export default FetchScholarInfo;
