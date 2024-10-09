bbbdbe13a1dc4c8e5a182c00edad8d4301b7f1ff
lxzLocus <75218672+lxzLocus@users.noreply.github.com>
2024-10-09 16:12:32 +0000

    [WIP] ログイン状態確認の実装

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,40 +1,34 @@
-import type {
-    GetServerSidePropsContext,
-    InferGetServerSidePropsType,
-} from "next"
-import { getProviders, signIn } from "next-auth/react"
-import { getServerSession } from "next-auth/next"
-import { authOptions } from "../../api/auth/[...nextauth]"
+// ./src/app/auth/signin/SignInClient.tsx
+"use client";  // クライアントコンポーネントとして宣言
 
-export default function SignIn({
-    providers,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-    return (
-        <>
-            {Object.values(providers).map((provider) => (
-                <div key={provider.name}>
-                    <button onClick={() => signIn(provider.id)}>
-                        Sign in with {provider.name}
-                    </button>
-                </div>
-            ))}
-        </>
-    )
-}
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Theme } from '@radix-ui/themes';
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-    const session = await getServerSession(context.req, context.res, authOptions)
+export default function SignInClient() {
+  const { data: session, status } = useSession();
+  const loading = status === "loading";
 
-    // If the user is already logged in, redirect.
-    // Note: Make sure not to redirect to the same page
-    // To avoid an infinite loop!
-    if (session) {
-        return { redirect: { destination: "/" } }
-    }
-
-    const providers = await getProviders()
-
-    return {
-        props: { providers: providers ?? [] },
-    }
+  return (
+    <Theme appearance="dark" accentColor="mint">
+      <div>
+        {!session && (
+          <>
+            {loading ? (
+              <>Loading ...</>
+            ) : (
+              <>
+                Not signed in <br />
+                <button onClick={() => signIn()}>Sign in</button>
+              </>
+            )}
+          </>
+        )}
+        {session && (
+          <>
+            <button onClick={() => signOut()}>Sign out</button>
+          </>
+        )}
+      </div>
+    </Theme>
+  );
 }
