df31751576159ee7b0919ed08f7e8853af1c1bed
lxzLocus <75218672+lxzLocus@users.noreply.github.com>
2024-10-10 05:44:34 +0000

    [WIP] ログイン処理

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -3,32 +3,69 @@
 
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Theme } from '@radix-ui/themes';
+import React, { useState, useEffect } from 'react';
 
 export default function SignInClient() {
   const { data: session, status } = useSession();
   const loading = status === "loading";
 
+    const [darkMode, setDarkMode] = useState(false);
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        const savedDarkMode = localStorage.getItem('darkMode');
+        if (savedDarkMode !== null) {
+            setDarkMode(JSON.parse(savedDarkMode));
+        }
+        setIsClient(true);
+    }, []);
+
+    useEffect(() => {
+        if (isClient) {
+            localStorage.setItem('darkMode', JSON.stringify(darkMode));
+        }
+    }, [darkMode, isClient]);
+
+    if (!isClient) {
+        return null;
+    }
+
   return (
-    <Theme appearance="dark" accentColor="mint">
-      <div>
-        {!session && (
-          <>
-            {loading ? (
-              <>Loading ...</>
-            ) : (
-              <>
-                Not signed in <br />
-                <button onClick={() => signIn()}>Sign in</button>
-              </>
-            )}
-          </>
-        )}
-        {session && (
-          <>
-            <button onClick={() => signOut()}>Sign out</button>
-          </>
-        )}
-      </div>
+      <Theme
+          appearance={darkMode ? 'dark' : 'light'}
+          accentColor="mint"
+          grayColor="gray"
+          panelBackground="solid"
+          scaling="100%"
+          radius="full"
+      >
+          <div>
+              {!session && (
+                  <>
+                      {loading ? (
+                          <>Loading ...</>
+                      ) : (
+                          <>
+                              Not signed in <br />
+                              <button onClick={() => signIn()}>Sign in</button>
+                          </>
+                      )}
+                  </>
+              )}
+              {session && (
+                  <>
+                      name:{session.user.name} <br />
+                      image:
+                      <img
+                          src={session.user.image}
+                          alt="icon"
+                          style={{ width: "100px", height: "100px" }}
+                      />
+                      <br />
+                      <button onClick={() => signOut()}>Sign out</button>
+                  </>
+              )}
+          </div>
     </Theme>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,43 @@
 import { SessionProvider } from "next-auth/react"; // SessionProvider をインポート
 import './globals.css'; // 他のグローバルスタイル
 import { Theme } from '@radix-ui/themes';
+import React, { useState, useEffect } from 'react';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [darkMode, setDarkMode] = useState(false);
+  const [isClient, setIsClient] = useState(false);
+
+
+  useEffect(() => {
+    const savedDarkMode = localStorage.getItem('darkMode');
+    if (savedDarkMode !== null) {
+      setDarkMode(JSON.parse(savedDarkMode));
+    }
+    setIsClient(true);
+  }, []);
+
+  useEffect(() => {
+    if (isClient) {
+      localStorage.setItem('darkMode', JSON.stringify(darkMode));
+    }
+  }, [darkMode, isClient]);
+
+  if (!isClient) {
+    return null;
+  }
+  
   return (
     <html lang="en">
       <body>
         <SessionProvider>
-          <Theme appearance="dark" accentColor="mint">
+          <Theme 
+            appearance={darkMode ? 'dark' : 'light'}
+            accentColor="mint"
+            grayColor="gray"
+            panelBackground="solid"
+            scaling="100%"
+            radius="full"
+          >
             {children}
           </Theme>
         </SessionProvider>
