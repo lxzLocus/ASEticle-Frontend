1c2359c8846f8d0690c8c6559123d451102d9659
lxzLocus <75218672+lxzLocus@users.noreply.github.com>
2024-10-12 15:44:16 +0000

    [WIP] wip

--- auth.ts
+++ auth.ts
@@ -1,21 +1,9 @@
 import NextAuth from "next-auth"
-// import log from "logging-service"
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
     providers: [GitHub, Google],
     debug: true,
-    // logger: {
-    //     error(code, ...message) {
-    //         log.error(code, message)
-    //     },
-    //     warn(code, ...message) {
-    //         log.warn(code, message)
-    //     },
-    //     debug(code, ...message) {
-    //         log.debug(code, message)
-    //     },
-    // }
 });
 
